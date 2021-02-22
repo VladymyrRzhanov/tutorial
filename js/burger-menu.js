bdda82ecc0296003073ce0012a4efeb389f08472
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-22 02:56:59 +0200

    Модалка

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,14 +1,29 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
+    openModalBtn: document.querySelector('[burger-menu-open]'),
+    closeModalBtn: document.querySelector('[burger-menu-close]'),
+    closeModalBurger: document.querySelector('[burger-menu-move]'),
+    // closeModalBurger: document.querySelector('[burger-menu-remove]'),
+    modal: document.querySelector('[burger-menu]'),
+    burger: document.querySelector('[burger-icon]')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBurger.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('menu--is-hidden');
   }
+
+  refs.openModalBtn.addEventListener('click', toggleBurger);
+  refs.closeModalBurger.addEventListener('click', toggleBurger);
+
+  function toggleBurger() {
+    refs.burger.classList.toggle('is-open');
+  }
+
+  // function removeModal() {
+  //   refs.modal.classList.remove('menu--is-hidden');
+  // }
 })();
